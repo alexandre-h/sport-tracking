736751078881d3b9147aed4dfb768f4643bec919
alexandre-h <herole.alexandre@gmail.com>
2024-03-14 14:56:45 +0100

    try subtree init

--- backend/app/models/seance.rb
+++
@@ -1,7 +0,0 @@
-class Seance < ApplicationRecord
-  belongs_to :user
-
-
-  validates :name, presence: true, uniqueness: true
-  validates :exercice, presence: true
-end
--- backend/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  require  "securerandom"
-  has_secure_password
-  has_many :seances
-
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :username, presence: true, uniqueness: true
-end
